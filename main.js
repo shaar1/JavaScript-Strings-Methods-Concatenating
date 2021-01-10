a2f381ee0a7fada72cb7589d8a29f4d7fcbd6c4a
Abdallah <abdallahchaar90@gmail.comh>
2021-01-10 20:50:42 +0100

    träning om en method som giller string och det är concatenating

---
+++ main.js
@@ -0,0 +1,23 @@
+/* global console, alert ,prompt*/
+
+
+/*
+string.fromCharCode(Num1, Num2, Num3 ,Num4)
+concat = concatenating (string, string, string)
+*/
+
+/*
+var myMessage = string.fromCharCode(65, 66, 65, 76, 76, 72);
+
+console.log(myMessage);
+*/
+
+var myMessage = "Hello I Love",
+
+myMessage2 = "PHP And JavaScript",
+
+myAll = myMessage.concat(myMessage2);
+
+console.log(myAll);
+
+
