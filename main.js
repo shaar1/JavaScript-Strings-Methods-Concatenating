e880776ee816a4b9a00026cf7f2b17fd7e43c861
Abdallah <abdallahchaar90@gmail.comh>
2021-01-10 20:57:05 +0100

    exampel om concatenating

--- main.js
+++ main.js
@@ -11,13 +11,46 @@ var myMessage = string.fromCharCode(65, 66, 65, 76, 76, 72);
 
 console.log(myMessage);
 */
-
+/*
 var myMessage = "Hello I Love",
 
-myMessage2 = "PHP And JavaScript",
+    myMessage2 = "PHP And JavaScript",
 
-myAll = myMessage.concat(myMessage2);
+    myAll = myMessage.concat(myMessage2);
 
 console.log(myAll);
+*/
 
+/*
+var myMessage = "Hello I Love",
+
+    myMessage2 = "PHP And JavaScript",
+
+    myAll = myMessage.concat(" " ,myMessage2);
+
+console.log(myAll);
+*/
+
+/*
+var myMessage = "Hello I Love",
+
+    myMessage2 = "PHP And JavaScript",
+
+    myAll = myMessage.concat(" ", myMessage2, " And Python");
+
+console.log(myAll);
+*/
+/*
+var myMessage = "Hello I Love",
+
+    myMessage2 = "PHP And JavaScript",
+
+    myAll = myMessage.concat(" ", myMessage2, " And Python");
+
+console.log(myAll);
+*/
+
+var myAll = "Hello I Love".concat(" ", "PHP And JavaScript", " And Python");
+
+console.log(myAll);
 
